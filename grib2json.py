a52888e72c90661b76503e66b3d8154b71d990cc
TomHeaven <tomheaven@qq.com>
2022-08-05 18:50:39 +0800

    add grib2json script

---
+++ grib2json.py
@@ -0,0 +1,50 @@
+"""
+The script requires grib2json to be installed correctly.
+Please refer to: https://github.com/cambecc/grib2json
+
+Author: TomHeaven.
+"""
+
+import os
+import glob
+import json
+import multiprocessing
+import numpy as np
+import tqdm
+
+DEBUG = False
+
+def main(file_path, out_path):
+    #print('file_path', file_path, 'out_path', out_path)
+    cmd = "grib2json -d -n -o {} {}".format(out_path, file_path)
+    os.system(cmd)
+
+if __name__ == '__main__':
+    serial = True
+    base = '.'
+    out = 'wind_json'
+    out_dir = os.path.join(base, out)
+    if not os.path.isdir(out_dir):
+        os.makedirs(out_dir)
+        
+    files = glob.glob(os.path.join(base, 'wind', '*.grib'))
+
+    idx = range(len(files))
+
+    if serial:
+        for f in tqdm.tqdm(idx):
+            out_path = files[f].replace("wind/",  out + "/")
+            out_path =  out_path.replace(".grib", '.json')
+            main(files[f], out_path)
+            #break
+    else:
+        pool = multiprocessing.Pool(multiprocessing.cpu_count() - 1)
+        out_path = files[idx].replace("wind/", out + "/")
+        out_path =  out_path.replace(".grib", '.json')
+        pool.map(main, files[idx])
+        pool.close()
+
+    files = glob.glob(os.path.join(base, out, '*.json'))
+    files = [os.path.split(i)[-1] for i in files]
+    with open(os.path.join(base, 'catalog_wind.json'), 'w') as f:
+         json.dump(np.sort(files).tolist(), f)
