84513a7f1b0b3de57b6035ab2e8fa808f1f3fa22
TomHeaven <tomheaven@qq.com>
2022-08-06 14:02:32 +0800

    now we can download forecast data every hour

--- wind_download.py
+++ wind_download.py
@@ -32,20 +32,23 @@ def download_wind(url, save_path, over_write=False):
 
 if __name__ == '__main__':
     grid = "0p25" # '0p25', '1p00'
-    base_url = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_{}.pl?file=gfs.t{}z.pgrb2.{}.f000&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&&dir=%2Fgfs.{}%2F{}%2Fatmos'
+    base_url = 'https://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_{}.pl?file=gfs.t{}z.pgrb2.{}.f0{}&lev_10_m_above_ground=on&var_UGRD=on&var_VGRD=on&&dir=%2Fgfs.{}%2F{}%2Fatmos'
     save_folder = 'wind'
     start_date = datetime.date.today()
     if not os.path.isdir(save_folder):
         os.makedirs(save_folder)
-    for i in range(0, 10):
-        for hour in ['00', '06', '12', '18']:
-            date = start_date - datetime.timedelta(days=i)
-            date_str = date.strftime("%Y%m%d")
-            print("Date", date_str, 'hour', hour)
-            url = base_url.format(grid, hour, grid, date_str, hour)
-            try: 
-                download_wind(url, os.path.join(save_folder, 'wind_' + date_str + '_' + hour + ".grib"))
-            except:
-                pass
+    for i in range(0, 10): # 过去天数
+        date = start_date - datetime.timedelta(days=i)
+        date_str = date.strftime("%Y%m%d")
+        for hour in [0, 6, 12, 18]: # 基准小时
+            hour_str = "%02d" % hour
+            for forecast in range(6): # 预报小时
+                real_hour = "%02d" % (hour + forecast)
+                print("Date", date_str, 'hour', real_hour)
+                url = base_url.format(grid, hour_str, grid, "%02d" % forecast, date_str, hour_str)
+                try: 
+                    download_wind(url, os.path.join(save_folder, 'wind_' + date_str + '_' + real_hour + ".grib"))
+                except:
+                    pass
                 
         
